3f767bb5a098d3855dba47bde266bfaffc3d1106
gustavo9601 <tavo9601@gmail.com>
2022-07-08 00:33:50 -0500

    stateless

--- src/main/java/org/gmarquez/webapp/ejb/service/ServiceEJB.java
+++ src/main/java/org/gmarquez/webapp/ejb/service/ServiceEJB.java
@@ -2,11 +2,15 @@ package org.gmarquez.webapp.ejb.service;
 
 import jakarta.ejb.Stateless;
 
-// Convierte en EJB
+// Convierte en EJB, pero sera un unico estado para todas las sesiones
 @Stateless
 public class ServiceEJB {
 
+    private Long contador = 0L;
     public String saludar(String nombre) {
+        System.out.println("Saludando desde instancia Stateless: " + this);
+        System.out.println("Contador: " + contador);
+        contador++;
         return "Hello World " + nombre + "!";
     }
 
